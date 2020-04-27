53d8b46f81af404647bcf844f6cb5daa26e57134
Arun sudharsan <sudarun@amazon.com>
2020-04-28 00:03:39 +0530

    Initial commit : Set Up with starter code from starter.spring.io

---
+++ src/main/java/io/arunbuilds/NotesWebServices/NotesWebServicesApplication.java
@@ -0,0 +1,13 @@
+package io.arunbuilds.NotesWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/arunbuilds/NotesWebServices/NotesWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.arunbuilds.NotesWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
