8bc3931e8c60ec61603518d01f2d166da2933e23
Arun sudharsan <sudarun@amazon.com>
2020-04-28 00:18:25 +0530

    Added PathVariable to GetMapping

--- src/main/java/io/arunbuilds/NotesWebServices/ui/Controllers/NoteController.java
+++ src/main/java/io/arunbuilds/NotesWebServices/ui/Controllers/NoteController.java
@@ -5,22 +5,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/notes")
 public class NoteController {
-    @GetMapping
-    String getNotes(){
+
+    @GetMapping(path = "/{noteId}")
+    String getNotes(@PathVariable String noteId) {
+        return "Getting your ".concat(noteId);
+    }
+
+    @GetMapping()
+    String getNotesEmpty() {
         return "Get";
     }
 
     @PostMapping
-    String addNotes(){
+    String addNotes() {
         return "post";
     }
+
     @PutMapping
-    String updateNotes(){
+    String updateNotes() {
         return "put";
     }
 
     @DeleteMapping
-    String deleteNotes(){
+    String deleteNotes() {
         return "delete";
     }
 
